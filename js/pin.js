eeceaefe5c14365b8b4d2046d4f180f4e61ac398
Vadim Rahmanov <VadimRahmanov@gmail.com>
2020-07-06 17:38:23 +0100

    Backend and success/error handlers

--- js/pin.js
+++ js/pin.js
@@ -3,25 +3,55 @@
 (function () {
   var pinTemplate = document.querySelector('#pin').content.querySelector('.map__pin');
   var mapPins = document.querySelector('.map__pins');
+  var errorTemplate = document.querySelector('#error').content.querySelector('.error');
+  var pins = [];
 
-  var renderPin = function (pins) {
+  var renderPin = function (pin) {
     var pinElement = pinTemplate.cloneNode(true);
     var pinImage = pinElement.querySelector('img');
-    pinElement.style.left = pins.location.x + 'px';
-    pinElement.style.top = pins.location.y + 'px';
-    pinImage.src = pins.author.avatar;
-    pinImage.alt = pins.offer.title;
+    pinElement.style.left = pin.location.x + 'px';
+    pinElement.style.top = pin.location.y + 'px';
+    pinImage.src = pin.author.avatar;
+    pinImage.alt = pin.offer.title;
     return pinElement;
   };
 
   var renderPinElements = function () {
-    var announcment = window.data.getAnnouncmentArray();
     var fragment = document.createDocumentFragment();
-    for (var i = 0; i < announcment.length; i++) {
-      fragment.appendChild(window.pin.renderPin(announcment[i]));
+    for (var i = 0; i < pins.length; i++) {
+      fragment.appendChild(renderPin(pins[i]));
     }
     mapPins.appendChild(fragment);
   };
+
+  var errorHandler = function () {
+    var error = errorTemplate.cloneNode(true);
+    var errorButton = error.querySelector('button');
+    document.body.insertAdjacentElement('afterbegin', error);
+
+    var closeErrorWindow = function () {
+      error.classList.add('hidden');
+      window.removeEventListener('click', closeErrorWindow);
+      errorButton.removeEventListener('click', closeErrorWindow);
+      document.removeEventListener('keydown', closeErrorWindowOnEscapePress);
+    };
+
+    var closeErrorWindowOnEscapePress = function (evt) {
+      if (evt.key === 'Escape') {
+        closeErrorWindow();
+      }
+    };
+
+    errorButton.addEventListener('click', closeErrorWindow);
+    window.addEventListener('click', closeErrorWindow);
+    document.addEventListener('keydown', closeErrorWindowOnEscapePress);
+  };
+
+  var successHandler = function (announcment) {
+    pins = announcment;
+  };
+
+  window.backend.load(successHandler, errorHandler);
 
   window.pin = {
     renderPin: renderPin,
